8f7db1b1fc03cb880e799fa80c3836e316aa9a6f
katarn <katarn@katarn.es>
2023-03-28 23:03:51 +0200

    Refactor: Remove comments in tests

--- python/tests/test_leap_year.py
+++ python/tests/test_leap_year.py
@@ -6,11 +6,7 @@ class TestLeapYear:
     def test_returns_false_if_year_not_divisible_by_4(
         self,
     ):
-        # Given
         leap_year = LeapYear()
-        # When
-
-        # Then
         assert leap_year.is_leap_year(1999) == False
 
     def test_returns_true_if_year_divisible_by_4(
