28c332b231ab1aa238664486ec41be492a71c9bd
Luz <luz.maria.agc@gmail.com>
2023-03-28 23:14:06 +0200

    Add forgotten test test_returns_false_if_year_not_divisible_by_4_like_2001

--- python/tests/test_leap_year.py
+++ python/tests/test_leap_year.py
@@ -3,11 +3,17 @@ from src.leap_year import LeapYear
 
 class TestLeapYear:
 
-    def test_returns_false_if_year_not_divisible_by_4(
+    def test_returns_false_if_year_not_divisible_by_4_like_1999(
         self,
     ):
         leap_year = LeapYear()
         assert leap_year.is_leap_year(1999) == False
+
+    def test_returns_false_if_year_not_divisible_by_4_like_2001(
+        self,
+    ):
+        leap_year = LeapYear()
+        assert leap_year.is_leap_year(2001) == False
 
     def test_returns_true_if_year_divisible_by_4_like_1996(
         self,
